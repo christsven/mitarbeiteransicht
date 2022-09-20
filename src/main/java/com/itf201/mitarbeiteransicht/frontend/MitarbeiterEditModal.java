a3c01aee7873a0ec9d00ce0992bdf209f7186c9d
christ_sve <chrsve@gmx.de>
2022-09-20 09:55:45 +0200

    current state of MitarbeiterEditModal

--- src/main/java/com/itf201/mitarbeiteransicht/frontend/MitarbeiterEditModal.java
+++ src/main/java/com/itf201/mitarbeiteransicht/frontend/MitarbeiterEditModal.java
@@ -1,6 +1,7 @@
 package com.itf201.mitarbeiteransicht.frontend;
 
 import com.itf201.mitarbeiteransicht.backend.MitarbeiterDto;
+import com.itf201.mitarbeiteransicht.backend.ReaderWriter;
 
 import javax.swing.*;
 import java.awt.*;
@@ -12,7 +13,7 @@ public class MitarbeiterEditModal extends JFrame {
     private static final Dimension BODY_SIZE = new Dimension(250, 350);
     private static final Dimension BUTTONROW_SIZE = new Dimension(500, 50);
     private static final Dimension BODY_OBJECT_SIZE = new Dimension(250, 50);
-    private static final Color BACKGROUND_COLOR = Color.GRAY;
+    private static final Color BACKGROUND_COLOR = Color.WHITE;
     private final JPanel header = new JPanel();
     private final JPanel body = new JPanel(new BorderLayout());
     private final JPanel bodyInternalLeft = new JPanel();
@@ -25,43 +26,65 @@ public class MitarbeiterEditModal extends JFrame {
     private final JLabel headerText = new JLabel();
     private final JLabel textId = new JLabel();
     private final JLabel textName = new JLabel();
-    private final JLabel textCustom1 = new JLabel();
-    private final JLabel textCustom2 = new JLabel();
+    private final JLabel textCustomUpper = new JLabel();
+    private final JLabel textCustomLower = new JLabel();
 
-    private final JTextField textFieldId = new JTextField();
-    private final JTextField textFieldName = new JTextField();
-    private final JTextField buttonCustom1 = new JTextField();
-    private final JTextField buttonCustom2 = new JTextField();
+    private final JLabel textFieldId = new JLabel();
+    private final JLabel textFieldName = new JLabel();
+    private final JTextField inputCustomUpper = new JTextField();
+    private final JTextField inputCustomLower = new JTextField();
 
-    public MitarbeiterEditModal(MitarbeiterDto dude) {
+    private final ReaderWriter readerWriter;
+
+    private MitarbeiterDto mitarbeiterToEdit;
+
+    public MitarbeiterEditModal(ReaderWriter readerWriter, MitarbeiterDto dude) {
+        this.readerWriter = new ReaderWriter();
+        mitarbeiterToEdit = dude;
+
         createHeader();
         createDefaultBody();
         createButtonRow();
-        switch (dude.typ()) {
-            case SCHICHTARBEITER -> createSchichtarbeiterBody(dude);
-            case BUEROARBEITER -> createBueroarbeiterBody(dude);
-            case MANAGER -> createManagerBody(dude);
+        switch (mitarbeiterToEdit.typ()) {
+            case SCHICHTARBEITER -> createSchichtarbeiterBody(mitarbeiterToEdit);
+            case BUEROARBEITER -> createBueroarbeiterBody(mitarbeiterToEdit);
+            case MANAGER -> createManagerBody(mitarbeiterToEdit);
         }
+        textFieldId.setText(String.valueOf(mitarbeiterToEdit.id()));
+        textFieldName.setText(mitarbeiterToEdit.name());
         this.setSize(WINDOW_SIZE);
         this.setResizable(false);
         this.setAlwaysOnTop(true);
         this.setVisible(true);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.pack();
+
+        updateButton.addActionListener(e -> saveToDb());
     }
 
     private void createManagerBody(MitarbeiterDto dto) {
-        textCustom1.setText("Festgehalt");
-        textCustom2.setText("Bonussatz");
-        textFieldId.setText(String.valueOf(dto.id()));
-        textFieldName.setText(dto.name());
-        buttonCustom1.setText(String.valueOf(dto.festgehalt()));
-        buttonCustom2.setText(String.valueOf(dto.bonussatz()));
+        textCustomUpper.setText("Festgehalt");
+        textCustomLower.setText("Bonussatz");
+        inputCustomUpper.setText(String.valueOf(dto.festgehalt()));
+        inputCustomLower.setText(String.valueOf(dto.bonussatz()));
     }
 
-    private void createBueroarbeiterBody(MitarbeiterDto dto) {}
+    private void createBueroarbeiterBody(MitarbeiterDto dto) {
+        textCustomUpper.setText("Festgehalt");
+        inputCustomUpper.setText(String.valueOf(dto.festgehalt()));
+        inputCustomLower.setVisible(false);
+        textCustomLower.setVisible(false);
+        //TODO leerzeile entfernen
+        this.remove(inputCustomLower);
+        this.remove(textCustomLower);
+    }
 
-    private void createSchichtarbeiterBody(MitarbeiterDto dto) {}
+    private void createSchichtarbeiterBody(MitarbeiterDto dto) {
+        textCustomUpper.setText("Stundenlohn");
+        inputCustomUpper.setText(String.valueOf(dto.stundenlohn()));
+        textCustomLower.setText("Stundenzahl");
+        inputCustomLower.setText(String.valueOf(dto.stundenzahl()));
+    }
 
     private void createHeader() {
         headerText.setFont(new Font(Font.SERIF, Font.BOLD, 20));
@@ -78,6 +101,7 @@ public class MitarbeiterEditModal extends JFrame {
         textName.setText("Name");
         textName.setVisible(true);
         textName.setSize(BODY_OBJECT_SIZE);
+        textName.setFocusable(false);
         textId.setText("ID");
         textId.setVisible(true);
         textId.setSize(BODY_OBJECT_SIZE);
@@ -85,31 +109,32 @@ public class MitarbeiterEditModal extends JFrame {
         //right side objects
         textFieldId.setSize(BODY_OBJECT_SIZE);
         textFieldName.setSize(BODY_OBJECT_SIZE);
-        //buttonCustom1.setSize(BODY_OBJECT_SIZE);
-        //buttonCustom2.setSize(BODY_OBJECT_SIZE);
+        inputCustomUpper.setSize(BODY_OBJECT_SIZE);
+        inputCustomLower.setSize(BODY_OBJECT_SIZE);
 
         body.setLayout(new BoxLayout(body, BoxLayout.X_AXIS));
         body.add(bodyInternalLeft);
         body.add(bodyInternalRight);
 
-        bodyInternalLeft.setLayout(new GridLayout(4,1));
+        bodyInternalLeft.setLayout(new GridLayout(4, 1));
         bodyInternalLeft.setSize(BODY_SIZE);
         bodyInternalLeft.add(textName);
         bodyInternalLeft.add(textId);
-        bodyInternalLeft.add(textCustom1);
-        bodyInternalLeft.add(textCustom2);
+        bodyInternalLeft.add(textCustomUpper);
+        bodyInternalLeft.add(textCustomLower);
         bodyInternalLeft.setBackground(BACKGROUND_COLOR);
 
-        bodyInternalRight.setLayout(new GridLayout(4,1));
+        bodyInternalRight.setLayout(new GridLayout(4, 1));
         bodyInternalRight.setSize(BODY_SIZE);
         bodyInternalRight.add(textFieldName);
         bodyInternalRight.add(textFieldId);
-        bodyInternalRight.add(buttonCustom1);
-        bodyInternalRight.add(buttonCustom2);
+        bodyInternalRight.add(inputCustomUpper);
+        bodyInternalRight.add(inputCustomLower);
         bodyInternalRight.setBackground(BACKGROUND_COLOR);
 
         body.setBackground(BACKGROUND_COLOR);
         body.setSize(BODY_SIZE);
+
 
         this.getContentPane().add(body, BorderLayout.CENTER);
     }
@@ -120,6 +145,7 @@ public class MitarbeiterEditModal extends JFrame {
 
         exitButton.setText("Schließen");
         exitButton.setVisible(true);
+        exitButton.addActionListener(e -> this.dispose());
 
         updateButton.setText("Speichern");
         updateButton.setVisible(true);
@@ -129,5 +155,41 @@ public class MitarbeiterEditModal extends JFrame {
 
         this.getContentPane().add(buttonrow, BorderLayout.SOUTH);
     }
+
+    //if all values are validated, user is able to save to """database"""
+    private boolean saveToDb() {
+        MitarbeiterDto result = switch (mitarbeiterToEdit.typ()) {
+            case MANAGER -> new MitarbeiterDto(
+                    mitarbeiterToEdit.id(),
+                    mitarbeiterToEdit.name(),
+                    mitarbeiterToEdit.typ(),
+                    Double.valueOf(inputCustomUpper.getText()),
+                    0.0,
+                    Double.valueOf(inputCustomLower.getText()),
+                    0
+            );
+            case BUEROARBEITER -> new MitarbeiterDto(
+                    mitarbeiterToEdit.id(),
+                    mitarbeiterToEdit.name(),
+                    mitarbeiterToEdit.typ(),
+                    Double.valueOf(inputCustomUpper.getText()),
+                    0.0,
+                    0.0,
+                    0
+            );
+            case SCHICHTARBEITER -> new MitarbeiterDto(
+                    mitarbeiterToEdit.id(),
+                    mitarbeiterToEdit.name(),
+                    mitarbeiterToEdit.typ(),
+                   0.0,
+                    Double.valueOf(inputCustomUpper.getText()),
+                    0.0,
+                    Integer.parseInt(inputCustomLower.getText())
+            );
+        };
+        readerWriter.createMitarbeiter(result);
+        return false;
+    }
+
 
 }
