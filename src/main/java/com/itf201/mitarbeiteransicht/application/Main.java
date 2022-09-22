d94aa977cc2185216c097d286774067ce14ed376
christ_sve <chrsve@gmx.de>
2022-09-22 08:43:09 +0200

    Switch json for empty CSVRepository

--- src/main/java/com/itf201/mitarbeiteransicht/application/Main.java
+++ src/main/java/com/itf201/mitarbeiteransicht/application/Main.java
@@ -1,15 +1,13 @@
 package com.itf201.mitarbeiteransicht.application;
 
-import com.itf201.mitarbeiteransicht.backend.MitarbeiterDto;
 import com.itf201.mitarbeiteransicht.backend.ReaderWriter;
-import com.itf201.mitarbeiteransicht.backend.person.MitarbeiterTyp;
-import com.itf201.mitarbeiteransicht.frontend.MitarbeiterEditModal;
 
 public class Main {
 
     public static void main(String[] args) {
         ReaderWriter readerWriter = new ReaderWriter();
-        MitarbeiterEditModal modal = new MitarbeiterEditModal(
+        readerWriter.getAllMitarbeiter();
+        /*MitarbeiterEditModal modal = new MitarbeiterEditModal(
                 readerWriter,
                 new MitarbeiterDto(
                         1,
@@ -18,7 +16,6 @@ public class Main {
                         1234.0,
                         69.0,
                         1000000.0,
-                        40));
-        System.out.println("Moin Servus Moin");
+                        40));*/
     }
 }
